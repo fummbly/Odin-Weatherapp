fc8cb3f1e951a88b737a0515c95c9b3d33d84ef6
fummbly <tstone449@gmail.com>
2023-04-26 13:59:37 -0400

    Started Api data manipulation

--- index.js
+++ index.js
@@ -2,13 +2,67 @@ const content = document.getElementById('content')
 
 const api_key = '8802b20c2a01fb90f6f23dfeba4d3866'
 
-const api_call = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8802b20c2a01fb90f6f23dfeba4d3866`
+const city_call = `http://api.openweathermap.org/geo/1.0/direct?q=Boston&appid=${api_key}`
 
-console.log(api_call)
+const weather_call = 'http://api.openweathermap.org/data/2.5/weather?'
+
+class Location {
+    constructor(name, lat, lon) {
+        this.name = name;
+        this.lat = lat;
+        this.lon = lon
+        this.weather = []
+    }
+
+    addWeather(forecast) {
+        this.weather.push(forecast)
+    }
+
+    getWeather() {
+        return this.weather
+    }
+
+    
+}
+
+class Forecast {
+    constructor(date, feels_like, tmp, weather ) {
+        this.date = date;
+        this.feels_like = feels_like;
+        this.tmp = tmp;
+        this.weather = weather
+    }
+
+    
+}
+
+function createLocation(data) {
+    const loc = data[0]
+
+    return new Location(loc.name, loc.lat, loc.lon)
+}
+
+function createWeather(data) {
+    const forecast = data
+
+    return new Forecast(forecast.dt_txt, forecast.main.feels_like, forecast.main.temp, forecast.weather)
+}
+
 async function getApiData() {
-    const response = await fetch(api_call, {mode: 'cors'})
-    const data = response.json()
-    console.log(data)
+    const city_response = await fetch(city_call, {mode: 'cors'})
+    const data = await city_response.json()
+    const currLocation = createLocation(data)
+
+    console.log(currLocation.name)
+    const weather_call = `http://api.openweathermap.org/data/2.5/forecast?lat=${currLocation.lat}&lon=${currLocation.lon}&appid=${api_key}&units=imperial`
+    const weather_response = await fetch(weather_call, {mode: 'cors'})
+    const weather_data = await weather_response.json()
+    weather_data['list'].forEach((timestamp) => {
+        console.log(timestamp)
+        currLocation.addWeather(createWeather(timestamp))
+    }) 
+
+    console.log(currLocation.weather)
 }
 
 
